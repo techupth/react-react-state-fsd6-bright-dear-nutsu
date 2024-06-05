0210cd42325ec90f75dc696af25b4b2f406228b8
Nat Tan <tresarpnat27@gmail.com>
2024-06-05 16:20:43 +0700

    solv excercise react state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+  const greetingThai = () => {
+    setGreeting("สวัสดี!");
+  };
+  const greetingEng = () => {
+    setGreeting("Hi!");
+  };
+  const greetingChina = () => {
+    setGreeting("你好!");
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={greetingThai}>สวัสดี!</button>
+        <button onClick={greetingEng}>Hi!</button>
+        <button onClick={greetingChina}>你好!</button>
       </div>
     </div>
   );
