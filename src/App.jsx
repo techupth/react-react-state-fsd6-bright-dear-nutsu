93444ed709259e73900f27b337309f6aac3f0d72
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-06-05 12:30:31 +0700

    feat: add message state and handleClick function on buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  const [greetingMessage, setGreetingMessage] = useState('Greeting Message');
+
+  const handleClick = (message) => setGreetingMessage(message);
+
+  // const messages = ['สวัสดี!', 'Hi!', '你好!'];
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        {/* Function Event Handler */}
+        <button onClick={() => handleClick('สวัสดี!')}>สวัสดี!</button>
+        <button onClick={() => handleClick('Hi!')}>Hi!</button>
+        <button onClick={() => handleClick('你好!')}>你好!</button>
+
+        {/* Anonymous Function  */}
+        {/* <button onClick={() => setGreetingMessage('สวัสดี!')}>สวัสดี!</button>
+        <button onClick={() => setGreetingMessage('Hi!')}>Hi!</button>
+        <button onClick={() => setGreetingMessage('你好!')}>你好!</button> */}
+
+        {/* Rendering List Items with Anonymous function  */}
+        {/* {messages.map((message, index) => {
+          return (
+            <button key={index} onClick={() => setGreetingMessage(message)}>
+              {message}
+            </button>
+          );
+        })} */}
       </div>
     </div>
   );
